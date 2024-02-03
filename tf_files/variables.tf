20a7b1cdcdca3b92d44d308df3bb243c3ff9b4b9
merverskn <emrverskn@gmail.com>
2024-02-04 00:01:29 +0300

    updated files

---
+++ tf_files/variables.tf
@@ -0,0 +1,13 @@
+//variable "aws_secret_key" {}
+//variable "aws_access_key" {}
+variable "region" {}
+variable "*****" {}
+variable "tags" {}
+variable "myami" {
+  description = "redhat enterprise linux 9 ami"
+}
+variable "instancetype" {}
+variable "controlinstancetype" {}
+variable "num" {}
+variable "mysecgr" {}
+variable "user" {}
