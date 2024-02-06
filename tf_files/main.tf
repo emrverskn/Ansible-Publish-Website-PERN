2942c4eb03798ec708e230930fcc6d5f945f1d02
Elif Merve Eriskin <140524629+emrverskn@users.noreply.github.com>
2024-02-06 12:53:08 +0300

    Update main.tf

--- tf_files/main.tf
+++ tf_files/main.tf
@@ -1,5 +1,5 @@
 
-//allowing SSH (22), 5000, 3000 and 5432 connecti
+//allowing SSH (22), 5000, 
 //User needs to select appropriate variables form "tfvars" file when launching the instance.
 
 terraform {
